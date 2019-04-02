90003d5b8402251b5b9c77cdd66467060b2db06b
TerenceLyu <34435235+TerenceLyu@users.noreply.github.com>
2019-04-02 12:16:50 -0400

    a

--- dist/Main.java
+++ dist/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
@@ -15,16 +16,44 @@ public class Main
 	public static void main(String[] args) throws IOException
 	{
 		Scanner input = new Scanner(System.in);
-		String[] filenames = input.next().split(",");
-		BufferedReader[] tableReader = new BufferedReader[filenames.length];
-		File[] f = new  File[filenames.length];
-		int index = 0;
-		for (String filename : filenames)
+		String[] filenames = input.nextLine().split(",");
+		HashMap<Character, String> names = new HashMap<>();
+		int letter = 65;
+		for (String s : filenames)
 		{
-			f[]
-			tableReader[index] = new BufferedReader(new FileReader(new File(filename)));
+			names.put((char) letter, s);
+			System.out.println(names.get((char) letter));
+			letter++;
 		}
+		int numberOfQueries = input.nextInt();
+		for (int i = 0; i < numberOfQueries; i++)
+		{
+			input.nextLine();//skip empty line
+			String select = input.nextLine().split(" ", 2)[1];
+			String from = input.nextLine().split(" ", 2)[1];
+			String where = input.nextLine().split(" ", 2)[1];
+			String and = input.nextLine().split(" ", 2)[1];
+			String[] tables = from.split(", ");
+			BufferedReader[] tableReader = new BufferedReader[tables.length];
+			for (int j = 0; j < tables.length; j++)
+			{
+				tableReader[j] = new BufferedReader(new FileReader(new File(names.get(tables[j].charAt(0)))));
+			}
+			
+			System.out.println(1 + select);
+			System.out.println(2 + from);
+			System.out.println(3 + where);
+			System.out.println(4 + and);
+		}
+//		BufferedReader[] tableReader = new BufferedReader[filenames.length];
+//		File[] f = new  File[filenames.length];
+//		int index = 0;
+//		for (String filename : filenames)
+//		{
+//			f[]
+//			tableReader[index] = new BufferedReader(new FileReader(new File(filename)));
+//		}
 		
 	}
-	public static
+//	public static
 }
--- dist/run.sh
+++ dist/run.sh
@@ -1,5 +1,2 @@
 # put commands here to run your program
 java Main
-# python3 Main.py
-# ./a.out
-
